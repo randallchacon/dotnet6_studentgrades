436d0043c8e3c9371a9e539cefd9e3f6e7fe9284
randallchacon <info@randallchacon.com>
2022-06-08 19:22:26 -0600

    Using partial views, and foreach with razor

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -11,10 +11,31 @@ namespace dotnet6_studentgrades.Controllers
                 UniqueId = Guid.NewGuid().ToString(),
                 Name = "C# programming",
             };
+
             ViewBag.DynamicData = "Dynamic information";
             ViewBag.Date = DateTime.Now;
 
             return View(subject);
         }
+        public IActionResult Multiple()
+        {
+            var SubjectList = new List<Subject>(){
+                new Subject{Name="Finance", UniqueId= Guid.NewGuid().ToString()},
+                new Subject{Name="Data bases", UniqueId= Guid.NewGuid().ToString()},
+                new Subject{Name="Web programming", UniqueId= Guid.NewGuid().ToString()},
+                new Subject{Name="HTML and CSS", UniqueId= Guid.NewGuid().ToString()},
+                new Subject{Name="Security", UniqueId= Guid.NewGuid().ToString()},
+            };
+
+            var subject = new Subject(){
+                UniqueId = Guid.NewGuid().ToString(),
+                Name = "C# programming",
+            };
+
+            ViewBag.DynamicData = "Dynamic information";
+            ViewBag.Date = DateTime.Now;
+
+            return View(SubjectList);
+        }        
     }
 }
